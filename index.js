d564a3192869c79feb7177f52bd5d2589053271f
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2021-11-03 09:21:27 -0600

    class file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const inquirer = require('inquirer')
+function creatmanager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "what is your first name?"
+        },
 
+    ]).then(function (answer) {
+        console.log(answer)
+    })
+}
+creatmanager();
